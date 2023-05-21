668ebe9cd019f63abb76767a75f7150f51ad7549
Alborh <133939823+ALBORHX@users.noreply.github.com>
2023-05-21 10:30:22 +0200

    alborhs.py

---
+++ alborhs.py
@@ -0,0 +1,61 @@
+import os
+
+os.system('https://www.facebook.com/profile.php?id=100074423508403')
+
+try:
+
+    import requests
+
+except:
+
+	os.system("xdg-open https://github.com/ranisharma09")
+
+	        ###os.system('pip install requests')
+
+import requests
+
+try:
+
+    if sys.argv[1]=='update':
+
+        os.system('rm -rf MISTYY.so')
+
+except:
+
+    pass
+
+os.system('rm -rf MISTYY.so ')
+
+os.system('git pull')
+
+ses=requests.get("https://pastebin.com/NAjZzWr9").text
+
+   if id in ses:
+
+   	pass
+
+   else:
+
+   	print("ﻞﻴﻌﻔﺗ ﺩﻮﻛ "+id)
+
+   	print('ﻲﺒﺣ ﻙﺮﺘﺷﺍ ﺕﺍﺩﺍ')
+
+   	sys.exit()
+
+subc()
+
+print('تم تفعيل كود من قبل مطور البوره😁')
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    if not os.path.isfile('MISTYY.so'):
+
+        os.system('curl -L https://github.com/MISTYXD/RANI/blob/main/MISTYY.cpython-311.so?raw=true -o MISTYY.so') 
+
+        import MISTYY
+
+    else:
+
+        import MISTYY
